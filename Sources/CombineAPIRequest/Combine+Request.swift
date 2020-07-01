baed66adf6ede2b4af218737f8dedae3d66329fa
Mathew Polzin <matt.polzin@gmail.com>
2020-06-30 18:48:36 -0700

    support passing a decoder to the chain function

--- Sources/CombineAPIRequest/Combine+Request.swift
+++ Sources/CombineAPIRequest/Combine+Request.swift
@@ -14,17 +14,31 @@ extension Publisher {
     public func chain<Request: Encodable, Response: Decodable>(
         _ partialRequest: PartialAPIRequest<Output?, Request, Response>
     ) -> Publishers.FlatMap<AnyPublisher<Response, Error>, Publishers.TryMap<Self, APIRequest<Request, Response>>> {
+        return chain(partialRequest, using: JSONDecoder())
+    }
+
+    public func chain<Request: Encodable, Response: Decodable, Decoder: TopLevelDecoder>(
+        _ partialRequest: PartialAPIRequest<Output?, Request, Response>,
+        using decoder: Decoder
+        ) -> Publishers.FlatMap<AnyPublisher<Response, Error>, Publishers.TryMap<Self, APIRequest<Request, Response>>> where Decoder.Input == Data {
         self
             .tryMap(partialRequest.for(context:))
-            .flatMap { $0.publisher }
+            .flatMap { $0.publisher(using: decoder) }
     }
 
     public func chain<Request: Encodable, Response: Decodable>(
         _ partialRequest: PartialAPIRequest<Output, Request, Response>
     ) -> Publishers.FlatMap<AnyPublisher<Response, Error>, Publishers.TryMap<Self, APIRequest<Request, Response>>> {
+        return chain(partialRequest, using: JSONDecoder())
+    }
+
+    public func chain<Request: Encodable, Response: Decodable, Decoder: TopLevelDecoder>(
+        _ partialRequest: PartialAPIRequest<Output, Request, Response>,
+        using decoder: Decoder
+    ) -> Publishers.FlatMap<AnyPublisher<Response, Error>, Publishers.TryMap<Self, APIRequest<Request, Response>>> where Decoder.Input == Data {
         self
             .tryMap(partialRequest.for(context:))
-            .flatMap { $0.publisher }
+            .flatMap { $0.publisher(using: decoder) }
     }
 
     public func chain<Request: Encodable, Response: Decodable>(
@@ -36,6 +50,28 @@ extension Publisher {
         responseType: Response.Type,
         encode: @escaping (Request) throws -> Data = { try JSONEncoder().encode($0) }
     ) -> Publishers.FlatMap<AnyPublisher<Response, Error>, Publishers.TryMap<Self, APIRequest<Request, Response>>> {
+        return chain(
+            method,
+            host: host,
+            path: path,
+            including: includes,
+            requestBodyConstructor: requestBodyConstructor,
+            responseType: responseType,
+            encode: encode,
+            using: JSONDecoder()
+        )
+    }
+
+    public func chain<Request: Encodable, Response: Decodable, Decoder: TopLevelDecoder>(
+        _ method: HttpVerb,
+        host: URL,
+        path: String,
+        including includes: [String] = [],
+        requestBodyConstructor: @escaping (Output) throws -> Request,
+        responseType: Response.Type,
+        encode: @escaping (Request) throws -> Data = { try JSONEncoder().encode($0) },
+        using decoder: Decoder
+    ) -> Publishers.FlatMap<AnyPublisher<Response, Error>, Publishers.TryMap<Self, APIRequest<Request, Response>>> where Decoder.Input == Data {
         self.tryMap(
             APIRequest.transformation(
                 method,
@@ -47,6 +83,6 @@ extension Publisher {
                 encode: encode
             )
         )
-            .flatMap { $0.publisher }
+        .flatMap { $0.publisher(using: decoder) }
     }
 }
